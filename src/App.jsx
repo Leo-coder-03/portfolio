1e8f47c39fd9503c46aaa01517f9e1a3ac5d2998
Shruti <snktiwari1@gmail.com>
2025-03-27 22:52:12 +0530

    arrallax added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/navbar";
+import Parallax from "./components/hero/parallax/Parallax";
 const App = () => {
   return <div>
     <section id="Homepage">
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Services</section>
-    <section id="Portfolio">Portfolio</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section>Services</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
     <section id="Contact">Contact</section>
     <section id="About">About</section>
   </div>;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,67 @@
-import "./hero.scss"
+import "./hero.scss";
+import { motion } from "framer-motion";
+const textVariants = {
+  initial:{
+    x:-500,
+    opacity:0,
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    transition:{
+      duration:1,
+      staggerChildren:0.1,
+    },
+  },
+  scrollButton:{
+    opacity:0,
+    y:10,
+    transition:{
+      duration:2,
+    repeat:Infinity,
+    }
+  }
+};
+const sliderVariants = {
+  initial:{
+    x:0,
+  },
+  animate:{
+    x:"-220%",
+    transition:{
+      repeat:Infinity,
+      repeatType:"mirror",
+      duration:20,
+      staggerChildren:0.1,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
+    <div className="wrapper">
+    <motion.div className="textContainer" variants={textVariants} initial="initial"animate="animate">
+      <motion.h2 variants={textVariants}>SHRUTI TIWARI</motion.h2>
+      <motion.h1 variants={textVariants}>FullStack Developer</motion.h1>
+      <motion.div variants={textVariants} className="buttons">
+        <button variants={textVariants}>See the Latest Work</button>
+        <button variants={textVariants} style={{backgroundColor:"white",color:"black"}}>Contact Me</button>
+      </motion.div>
+      <motion.img variants={textVariants} src="/scroll.png"alt="" initial="hidden" animate="scrollButton"></motion.img>
+    </motion.div>
+    </div>
+    <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Coding Development Technology
+      </motion.div>
+
       <div className="imageContainer">
-        <img src="/hero.png"alt=""/>
+        <img src="/hero.png" alt="Hero Image" />
       </div>
     </div>
   )
